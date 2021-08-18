3c2cae6d0ba4ac76305101a3ac379a43effb4d05
kevinmore26 <74431425+kevinmore26@users.noreply.github.com>
2021-08-17 23:18:24 -0500

    ultimo

--- src/Routes.js
+++ src/Routes.js
@@ -7,12 +7,14 @@ import PortadaView from './views/PortadaView'
 import IniciarSecionView from "./views/IniciarSecionView";
 import RegistroView  from "./views/RegistroView";
 import PlanesKev from "./views/PlanesKev"
+import Checkout from './views/Checkout'
 
 
 export default function Routes ()
 {return(
     <div>
        <Route path="/" exact component={PortadaView} />
+       <Route path="/Checkout" exact component={Checkout} />
        <Route path="/Donar" exact component={PlanesKev} />
        <Route path="/Adoptar" exact component={PortadaViewAdopciones} />
        <Route path="/Tienda" exact component={PortadaViewArticulos} />
