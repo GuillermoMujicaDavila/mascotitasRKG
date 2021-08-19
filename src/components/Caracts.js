91a7a4946322f9e50841be9ae67fc905dfeb79e5
zen-rox93 <84250823+zen-rox93@users.noreply.github.com>
2021-08-19 01:33:17 -0500

    Mejora de texto

--- src/components/Caracts.js
+++ src/components/Caracts.js
@@ -26,46 +26,27 @@ export default function Caracts() {
                 margin: '20px',
                 textAlign:'center',
                 
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-
-               
-                
-                
             }}>
                 <div className="icono1">
-                    <img src={patitalogo} style={{
-                        width: '33%',
+                    <img src={patitalogo} alt="patitalogo" style={{
+                        
                         margin:'20px 0',
                         height: '40px',
-                        width: '40px',
                         filter: 'invert(0.4) sepia(1) hue-rotate(20deg)',
                         
-                        
-                        
-
                     }}/>
                     <h4 style={{
                         fontWeight: 'bold',
                         color: 'orange',
                     }}>Adopciones</h4>
-                    <p>Realizamos campañas de adopciones exitosas</p>
+                    <p className="letrita">Realizamos campañas de adopciones exitosas</p>
                 </div>
 
                 <div className="icono2">
-                    <img src={carritologo} style={{
-                        width: '33%',
+                    <img src={carritologo} alt="carritologo" style={{
+                        
                         margin:'20px 0',
                         height: '40px',
-                        width: '40px',
                         filter: 'invert(0.4) sepia(1) hue-rotate(20deg)',
                         
                     }}/>
@@ -73,38 +54,36 @@ export default function Caracts() {
                         fontWeight: 'bold',
                         color: 'orange',
                     }}>Ecommerce</h4>
-                    <p>Contamos con los mejores productos</p>
+                    <p className="letrita">Contamos con los mejores productos</p>
                 </div>
 
                 <div className="icono3">
-                    <img src={huesologo} style={{
-                        width: '33%',
+                    <img src={huesologo} alt="huesologo" style={{
+                       
                         margin:'20px 0',
                         height: '40px',
-                        width: '40px',
                         filter: 'invert(0.4) sepia(1) hue-rotate(20deg)',
                     }}/>
                     <h4 style={{
                         fontWeight: 'bold',
                         color: 'orange',
                     }}>Sponsor</h4>
-                    <p>Tenemos los mejores artículos premium
+                    <p className="letrita">Tenemos los mejores artículos premium
                     </p>
                 </div>
 
                 <div className="icono4">
-                    <img src={eventologo} style={{
-                        width: '33%',
+                    <img src={eventologo} alt="eventologo" style={{
+                      
                         margin:'20px 0',
                         height: '40px',
-                        width: '40px',
                         filter: 'invert(0.4) sepia(1) hue-rotate(20deg)',
                     }}/>
                     <h4 style={{
                         fontWeight: 'bold',
                         color: 'orange',
                     }}>Eventos</h4>
-                    <p>Planificamos eventos masivos para mascotas</p>
+                    <p className="letrita">Planificamos eventos masivos para mascotas</p>
                 </div>
 
 
